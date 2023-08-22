c9b95fd715799984c7add732cabfba4a9d0df2c8
omarjafor <omarjaforchy@gmail.com>
2023-08-22 12:44:58 +0600

    td tbody

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const taskButton = document.getElementById('task-btn');
 const inputField = document.getElementById('input-value');
-taskButton.addEventListener('click', function(){
-    console.log(inputField.value);
+let count = 0;
+taskButton.addEventListener('click', function () {
+    const inputValue = inputField.value;
+    const container = document.getElementById('content-container');
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+    <th>${(count += 1)}</th>
+    <td>${inputValue}</td>
+    <td>
+        <button class="done-btn btn btn-success btn-xs">Done</button>
+        <button class="delete-btn btn btn-error btn-xs">Delete</button>
+    </td>
+    `;
+    container.appendChild(tr);
 })
